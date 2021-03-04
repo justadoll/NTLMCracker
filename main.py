2461d5d4e5b23c4cf8a2e5915446171e76ed936e
justadoll <stelzybol@protonmail.com>
2021-03-04 05:25:45 -0600

    Limited Acces

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import logging
+#import logging
 from aiogram import Bot, types
 from aiogram.utils.helper import Helper
 from aiogram.utils import executor
@@ -10,15 +10,36 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from os import mkdir, getcwd, system
 import urllib
 from loguru import logger
+import sqlite3 as sq
+
 
 from config import TOKEN
 PATH = getcwd()
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 logger.add("debug.json", format="{time} {level} {message}", level="INFO", rotation="5 MB", compression="zip", serialize=True)
+
+def db_checker(id):
+    with sq.connect("db.db") as con:
+        cur = con.cursor()
+        select = f"SELECT id FROM users WHERE id == {id}"
+        cur.execute(select)
+        res = cur.fetchall()
+        if res != []:
+            return True
+        else:
+            return False
+
+
+def add_user(id):
+    with sq.connect("db.db") as con:
+        cur = con.cursor()
+        insert = f"INSERT INTO users VALUES({id},datetime('now','localtime'))"
+        cur.execute(insert)
+
 
 def make_dir(dirname):
     try:
@@ -36,11 +57,28 @@ class Form(StatesGroup):
     HASH = State()
 
 
+class Users(StatesGroup):
+    user_id = State()
+
 @dp.message_handler(commands=['start','help'])
 async def start_procces(msg:types.Message):
     await bot.send_message(msg.chat.id, "Hello, this bot was created for automatic brutforce Windows hashes")
-    #f = open("test.txt","rb")
-    #await bot.send_document(msg.chat.id,f)
+
+@dp.message_handler(commands=['add_user'])
+async def process_add(message: types.Message):
+    if message.chat.id == 450047498:
+        await Users.user_id.set()
+        await bot.send_message(message.chat.id, "Кого будем добавлять? (Только ID)")
+    else:
+        await bot.send_message(message.chat.id, "Ты не админ, кыш")
+
+
+@dp.message_handler(state=Users.user_id)
+async def process_test(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['user_id'] = message.text
+        id = int(data['user_id'])
+        add_user(id)
 
 @dp.message_handler(commands=['brute'])
 async def recv_message(message: types.Message):
@@ -50,18 +88,22 @@ async def recv_message(message: types.Message):
 @dp.message_handler(state=Form.name)
 async def process_test(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['name'] = message.text
-        if make_dir('files/'+data['name']):
-            await bot.send_message(message.chat.id, "Directory was created successfuly!")
+        if db_checker(message.chat.id) == False:
+            await bot.send_message(message.chat.id, "I didn`t know who are u...\n-_-")
+            await state.finish()
         else:
-            await bot.send_message(message.chat.id, "Directory name is invalid!")
+            data['name'] = message.text
+            if make_dir('files/'+data['name']):
+                await bot.send_message(message.chat.id, "Папка была создана успешно!")
+            else:
+                await bot.send_message(message.chat.id, "Название папки не подходит.\nHапишите /brute и попробуйте ещё раз")
 
-        global active_dir_path
-        active_dir_path = PATH+'/'+'files/'+data['name']
-        logger.info("WHOIS DIR : "+str(message.chat.id)+'\n'+str(message.chat.username)+'\n'+active_dir_path)
+            global active_dir_path
+            active_dir_path = PATH+'/'+'files/'+data['name']
+            logger.info("WHOIS DIR : "+str(message.chat.id)+'\n'+str(message.chat.username)+'\n'+active_dir_path)
 
-        await Form.next()
-        await message.reply("Send me a SAM file :^)")
+            await Form.next()
+            await message.reply("Пришлите SAM файл :^)\n!!! Имена файлов должны быть в верхнем регистре !!!")
 
 @dp.message_handler(lambda message: message.document.file_name == "SAM",content_types=['document'], state=Form.SAM)
 async def process_SAM(message: types.Message, state:FSMContext):
@@ -72,10 +114,10 @@ async def process_SAM(message: types.Message, state:FSMContext):
         fi = file_info.file_path
         name = message.document.file_name
         urllib.request.urlretrieve(f'https://api.telegram.org/file/bot{TOKEN}/{fi}',f'{active_dir_path}/{name}')
-        await bot.send_message(message.from_user.id, f'DEBUG: Файл SAM успешно сохранён {active_dir_path}/{name}')
+        await bot.send_message(message.from_user.id, f'DEBUG: Файл SAM успешно сохранён в {active_dir_path}/{name}')
 
     await Form.next()
-    await message.reply("Ok, SAM was uploaded\nNext, send me a SYSTEM file :^)")
+    await message.reply("Хорошо, SAM загружен\nТеперь скиньте файл SYSTEM :^)")
 
 @dp.message_handler(lambda message: message.document.file_name == "SYSTEM",content_types=['document'], state=Form.SYSTEM)
 async def process_SYSTEM(message: types.Message, state:FSMContext):
